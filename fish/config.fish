1d54db0faa427fc604a7efec3e5c7552eeff824c
InitialMoon <jieyingqi814@qq.com>
2024-09-12 13:24:46 +0800

    add proxy args

--- fish/config.fish
+++ fish/config.fish
@@ -11,7 +11,7 @@ function fish_user_key_bindings
 end
 
 function proxy_on
-    set -x http_proxy http://127.0.0.1:63572
+    set -x http_proxy http://127.0.0.1:$argv[1]
     set -x https_proxy $http_proxy
     echo "终端代理已开启。注意端口号需要和本机进行手动适配，请找到 config.fish 文件中的 proxy_on 函数进行修改"
     echo "proxy_off to close"
@@ -24,6 +24,17 @@ function proxy_off
 end
 
 echo "开启网络代理请使用proxy_on命令"
+
+# Created by `pipx` on 2024-08-21 12:48:44
+# Add user software to env path
+set PATH $PATH ~/.local/bin
+
+# nodejs18 setting
+
+# fish_add_path /home/linuxbrew/.linuxbrew/opt/node@18/bin
+# set -gx LDFLAGS "-L/home/linuxbrew/.linuxbrew/opt/node@18/lib"
+# set -gx CPPFLAGS "-I/home/linuxbrew/.linuxbrew/opt/node@18/include"
+
 # sudo service binfmt-support start
 
 # >>> conda initialize >>>
@@ -43,13 +54,3 @@ echo "开启网络代理请使用proxy_on命令"
 # set -Ux PATH $PYENV_ROOT/bin $PATH
 # set -Ux PATH /usr/local/nodejs/bin
 
-
-# Created by `pipx` on 2024-08-21 12:48:44
-# Add user software to env path
-set PATH $PATH ~/.local/bin
-
-# nodejs18 setting
-
-# fish_add_path /home/linuxbrew/.linuxbrew/opt/node@18/bin
-# set -gx LDFLAGS "-L/home/linuxbrew/.linuxbrew/opt/node@18/lib"
-# set -gx CPPFLAGS "-I/home/linuxbrew/.linuxbrew/opt/node@18/include"
